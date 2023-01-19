4fe5170e99e9404a79336e1c2710975ad257153a
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-01-19 10:10:59 +0100

    ok mais pas comme ile faut

--- LeetCodeTraining/Medium/ZigzagConversion.cs
+++ LeetCodeTraining/Medium/ZigzagConversion.cs
@@ -1,10 +1,18 @@
-﻿namespace LeetCodeTraining.Medium;
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCodeTraining.Medium;
 
 public static partial class Solution
 {
-    public static string ZigzagConversion(string s, int numRows)
+    public static string ZigzagConversion(string s)
     {
-
-        return "";
+        const string pattern = @"[ 
+]";
+        const string substitution = @"";
+        const RegexOptions options = RegexOptions.Multiline;
+        
+        var regex = new Regex(pattern, options);
+        
+        return regex.Replace(s, substitution);
     }
 }
--- LeetCodeTrainingTest/Medium/ZigzagConversion.cs
+++ LeetCodeTrainingTest/Medium/ZigzagConversion.cs
@@ -1,4 +1,4 @@
-using LeetCodeTraining.Hard;
+using LeetCodeTraining.Medium;
 using Xunit.Abstractions;
 
 namespace LeetCodeTrainingTest.Medium;
@@ -8,6 +8,7 @@ public class ZigzagConversion
     #region Statements
 
     private readonly ITestOutputHelper _print;
+    private static string _s = "";
 
     public ZigzagConversion(ITestOutputHelper print)
     {
@@ -17,10 +18,39 @@ public class ZigzagConversion
     #endregion
 
     //
+    
+    private string GetResult()
+    {
+        var result = Solution.ZigzagConversion(_s);
+        _print.WriteLine($"{result}");
+
+        return result;
+    }
 
     [Fact]
     public void Test1()
     {
+        _s = """
+    P   A   H   N
+    A P L S I I G
+    Y   I   R
+    """;
         
+        var result = GetResult();
+        Assert.True(result.Equals("PAHNAPLSIIGYIR"));
+    }
+    
+    [Fact]
+    public void Test2()
+    {
+        _s = """
+    P     I    N
+    A   L S  I G
+    Y A   H R
+    P     I
+    """;
+        
+        var result = GetResult();
+        Assert.True(result.Equals("PINALSIGYAHRPI"));
     }
 }
