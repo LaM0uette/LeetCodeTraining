58b100e5d8ec74c586313ee49fd49d630f0fcc2f
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-07 10:20:13 +0100

    test convertion ok

--- LeetCodeTraining/Easy/RomanToInteger.cs
+++ LeetCodeTraining/Easy/RomanToInteger.cs
@@ -1,24 +1,53 @@
-﻿namespace LeetCodeTraining.Easy;
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCodeTraining.Easy;
 
 public class RomanToInteger
 {
+    private const string pattern = @"(?:IV|IX|XL|XC|CD|CM|I|V|X|L|C|D|M)";
+    
     public int RomanToInt(string s)
+    {
+        if (!CheckLength(s)) throw new Exception();
+        
+        return RegexRomanToInt(s);
+    }
+
+    private static bool CheckLength(string s)
+    {
+        return s.Length is >= 1 and <= 15;
+    }
+
+    private static int RegexRomanToInt(string s)
+    {
+        var num = 0;
+        
+        foreach (Match m in Regex.Matches(s, pattern, RegexOptions.Multiline))
+        {
+            num += GetRomanToInt(m.Value);
+        }
+
+        return num;
+    }
+
+    private static int GetRomanToInt(string s)
     {
         return s switch
         {
             "I" => 1,
-            "II" => 2,
-            "III" => 3,
-            "IIII" => 4,
             "IV" => 4,
             "V" => 5,
-            "VI" => 6,
-            "VII" => 7,
-            "VIII" => 8,
-            "VIIII" => 9,
-            "VIV" => 9,
-            "VIIIII" => 10,
-            "VV" => 10,
+            "IX" => 9,
+            "X" => 10,
+            "XL" => 40,
+            "L" => 50,
+            "XC" => 90,
+            "C" => 100,
+            "CD" => 400,
+            "D" => 500,
+            "CM" => 900,
+            "M" => 1000,
+            _ => throw new ArgumentOutOfRangeException(nameof(s), s, null)
         };
     }
 }
--- LeetCodeTrainingTest/Easy/RomanToIntegerTest.cs
+++ LeetCodeTrainingTest/Easy/RomanToIntegerTest.cs
@@ -4,14 +4,6 @@ namespace LeetCodeTrainingTest.Easy;
 
 public class RomanToIntegerTest
 {
-    /*
-     Assert.Equal(3, RomanToInt("III"));
-     Assert.Equal(4, RomanToInt("IV"));
-     Assert.Equal(9, RomanToInt("IX"));
-     Assert.Equal(58, RomanToInt("LVIII"));
-     Assert.Equal(1994, RomanToInt("MCMXCIV"));
-     */
-
     private readonly RomanToInteger RomanToInteger = new RomanToInteger();
     
     [Fact]
@@ -20,15 +12,40 @@ public class RomanToIntegerTest
         Assert.Equal(1, RomanToInteger.RomanToInt("I"));
         Assert.Equal(2, RomanToInteger.RomanToInt("II"));
         Assert.Equal(3, RomanToInteger.RomanToInt("III"));
-        Assert.Equal(4, RomanToInteger.RomanToInt("IIII"));
         Assert.Equal(4, RomanToInteger.RomanToInt("IV"));
         Assert.Equal(5, RomanToInteger.RomanToInt("V"));
         Assert.Equal(6, RomanToInteger.RomanToInt("VI"));
         Assert.Equal(7, RomanToInteger.RomanToInt("VII"));
         Assert.Equal(8, RomanToInteger.RomanToInt("VIII"));
-        Assert.Equal(9, RomanToInteger.RomanToInt("VIIII"));
-        Assert.Equal(9, RomanToInteger.RomanToInt("VIV"));
-        Assert.Equal(10, RomanToInteger.RomanToInt("VIIIII"));
-        Assert.Equal(10, RomanToInteger.RomanToInt("VV"));
+        Assert.Equal(9, RomanToInteger.RomanToInt("IX"));
+        Assert.Equal(10, RomanToInteger.RomanToInt("X"));
     }
+    
+    [Fact]
+    public void RomanToInt_BigRoman_BigInt()
+    {
+        Assert.Equal(30, RomanToInteger.RomanToInt("XXX"));
+        Assert.Equal(40, RomanToInteger.RomanToInt("XL"));
+        Assert.Equal(50, RomanToInteger.RomanToInt("L"));
+        Assert.Equal(80, RomanToInteger.RomanToInt("LXXX"));
+        Assert.Equal(90, RomanToInteger.RomanToInt("XC"));
+        Assert.Equal(100, RomanToInteger.RomanToInt("C"));
+        Assert.Equal(800, RomanToInteger.RomanToInt("DCCC"));
+        Assert.Equal(900, RomanToInteger.RomanToInt("CM"));
+        Assert.Equal(1000, RomanToInteger.RomanToInt("M"));
+    }
+    
+    [Fact]
+    public void RomanToInt_LVIII_58()
+    {
+        Assert.Equal(58, RomanToInteger.RomanToInt("LVIII"));
+    }
+    
+    [Fact]
+    public void RomanToInt_MCMXCIV_1994()
+    {
+        Assert.Equal(1994, RomanToInteger.RomanToInt("MCMXCIV"));
+    }
+    
+    
 }
