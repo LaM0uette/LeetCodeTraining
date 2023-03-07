227d5e09a421a3ce315fa20eb77d92041b60da44
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-07 09:35:41 +0100

    test de 1 à 10

--- LeetCodeTraining/Easy/RomanToInteger.cs
+++ LeetCodeTraining/Easy/RomanToInteger.cs
@@ -4,6 +4,21 @@ public class RomanToInteger
 {
     public int RomanToInt(string s)
     {
-        throw new InvalidOperationException();
+        return s switch
+        {
+            "I" => 1,
+            "II" => 2,
+            "III" => 3,
+            "IIII" => 4,
+            "IV" => 4,
+            "V" => 5,
+            "VI" => 6,
+            "VII" => 7,
+            "VIII" => 8,
+            "VIIII" => 9,
+            "VIV" => 9,
+            "VIIIII" => 10,
+            "VV" => 10,
+        };
     }
 }
--- LeetCodeTrainingTest/Easy/RomanToIntegerTest.cs
+++ LeetCodeTrainingTest/Easy/RomanToIntegerTest.cs
@@ -15,8 +15,20 @@ public class RomanToIntegerTest
     private readonly RomanToInteger RomanToInteger = new RomanToInteger();
     
     [Fact]
-    public void RomanToInt_III_3()
+    public void RomanToInt_Roman_Int()
     {
+        Assert.Equal(1, RomanToInteger.RomanToInt("I"));
+        Assert.Equal(2, RomanToInteger.RomanToInt("II"));
         Assert.Equal(3, RomanToInteger.RomanToInt("III"));
+        Assert.Equal(4, RomanToInteger.RomanToInt("IIII"));
+        Assert.Equal(4, RomanToInteger.RomanToInt("IV"));
+        Assert.Equal(5, RomanToInteger.RomanToInt("V"));
+        Assert.Equal(6, RomanToInteger.RomanToInt("VI"));
+        Assert.Equal(7, RomanToInteger.RomanToInt("VII"));
+        Assert.Equal(8, RomanToInteger.RomanToInt("VIII"));
+        Assert.Equal(9, RomanToInteger.RomanToInt("VIIII"));
+        Assert.Equal(9, RomanToInteger.RomanToInt("VIV"));
+        Assert.Equal(10, RomanToInteger.RomanToInt("VIIIII"));
+        Assert.Equal(10, RomanToInteger.RomanToInt("VV"));
     }
 }
