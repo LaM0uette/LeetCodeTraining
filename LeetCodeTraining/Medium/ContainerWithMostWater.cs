97e4cee6e29a87425b8aa41fbe5e2865f5d3b424
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-18 17:46:54 +0100

    ajout + résoluition partiel de ContainerWithMostWater

---
+++ LeetCodeTraining/Medium/ContainerWithMostWater.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeTraining.Medium;
+
+public class ContainerWithMostWater
+{
+    public int MaxArea(int[] height)
+    {
+        var maxArea = 0;
+        var left = 0;
+        var right = height.Length - 1;
+
+        while (left < right)
+        {
+            var minHeight = Math.Min(height[left], height[right]);
+            var area = minHeight * (right - left);
+            
+            if (area > maxArea) maxArea = area;
+            
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+
+        return maxArea;
+    }
+}
---
+++ LeetCodeTrainingTest/Medium/ContainerWithMostWaterTest.cs
@@ -0,0 +1,29 @@
+﻿using LeetCodeTraining.Medium;
+
+namespace LeetCodeTrainingTest.Medium;
+
+public class ContainerWithMostWaterTest
+{
+    private ContainerWithMostWater ContainerWithMostWater { get; } = new();
+    private int GetMaxArea(int[] height) => ContainerWithMostWater.MaxArea(height);
+    
+    [Fact]
+    public void MaxArea_11_1() => 
+        Assert.Equal(1, GetMaxArea(new[] {1, 1}));
+    
+    [Fact]
+    public void MaxArea_123_2() => 
+        Assert.Equal(2, GetMaxArea(new[] {1, 2, 3}));
+    
+    [Fact]
+    public void MaxArea_223_4() => 
+        Assert.Equal(4, GetMaxArea(new[] {2, 2, 3}));
+    
+    [Fact]
+    public void MaxArea_323_6() => 
+        Assert.Equal(6, GetMaxArea(new[] {3, 2, 3}));
+    
+    [Fact]
+    public void MaxArea_186254837_49() => 
+        Assert.Equal(49, GetMaxArea(new[] {1, 8, 6, 2, 5, 4, 8, 3, 7}));
+}
