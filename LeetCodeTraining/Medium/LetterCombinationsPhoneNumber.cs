7863683487fdb4d51a0537005cd58d986d6618cf
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-22 13:42:10 +0100

    add basics tests

--- LeetCodeTraining/Medium/LetterCombinationsPhoneNumber.cs
+++ LeetCodeTraining/Medium/LetterCombinationsPhoneNumber.cs
@@ -4,6 +4,14 @@ public class LetterCombinationsPhoneNumber
 {
     public IList<string> LetterCombinations(string digits)
     {
-        return new List<string>();
+        var lst = new List<string>();
+
+        switch (digits)
+        {
+            case "2": lst = new List<string> {"a", "b", "c"}; break;
+            case "3": lst = new List<string> {"d", "e", "f"}; break;
+        }
+
+        return lst;
     }
 }
--- LeetCodeTrainingTest/Medium/LetterCombinationsPhoneNumberTest.cs
+++ LeetCodeTrainingTest/Medium/LetterCombinationsPhoneNumberTest.cs
@@ -5,12 +5,26 @@ namespace LeetCodeTrainingTest.Medium;
 public class LetterCombinationsPhoneNumberTest
 {
     private static readonly LetterCombinationsPhoneNumber _combinations = new();
-    public static IList<string> GetCombinations(string digits) => 
+    private static IList<string> GetCombinations(string digits) => 
         _combinations.LetterCombinations(digits);
 
     [Fact]
     public void LetterCombinations_null_()
     {
         Assert.Equal(new List<string>(), GetCombinations(""));
+    }
+    
+    [Fact]
+    public void LetterCombinations_2_abc()
+    {
+        var lst = new List<string>{"a", "b", "c"};
+        Assert.Equal(lst, GetCombinations("2"));
+    }
+    
+    [Fact]
+    public void LetterCombinations_3_def()
+    {
+        var lst = new List<string>{"d", "e", "f"};
+        Assert.Equal(lst, GetCombinations("3"));
     }
 }
