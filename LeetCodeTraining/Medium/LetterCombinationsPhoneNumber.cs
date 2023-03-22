3c9c539d759e43bdeb5a3f74886b66cd056683a7
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-22 13:35:01 +0100

    add LetterCombinationsPhoneNumber

---
+++ LeetCodeTraining/Medium/LetterCombinationsPhoneNumber.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCodeTraining.Medium;
+
+public class LetterCombinationsPhoneNumber
+{
+    public IList<string> LetterCombinations(string digits)
+    {
+        return new List<string>();
+    }
+}
---
+++ LeetCodeTrainingTest/Medium/LetterCombinationsPhoneNumberTest.cs
@@ -0,0 +1,16 @@
+﻿using LeetCodeTraining.Medium;
+
+namespace LeetCodeTrainingTest.Medium;
+
+public class LetterCombinationsPhoneNumberTest
+{
+    private static readonly LetterCombinationsPhoneNumber _combinations = new();
+    public static IList<string> GetCombinations(string digits) => 
+        _combinations.LetterCombinations(digits);
+
+    [Fact]
+    public void LetterCombinations_null_()
+    {
+        Assert.Equal(new List<string>(), GetCombinations(""));
+    }
+}
