7d6f4225974b2fb315a68d600ed47eea81d74e28
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-22 14:01:12 +0100

    factorize

--- LeetCodeTraining/Medium/LetterCombinationsPhoneNumber.cs
+++ LeetCodeTraining/Medium/LetterCombinationsPhoneNumber.cs
@@ -2,22 +2,22 @@
 
 public class LetterCombinationsPhoneNumber
 {
+    private Dictionary<string, List<string>> digitToListMap = new(){
+        { "2", new List<string> {"a", "b", "c"} },
+        { "3", new List<string> {"d", "e", "f"} },
+        { "4", new List<string> {"g", "h", "i"} },
+        { "5", new List<string> {"j", "k", "l"} },
+        { "6", new List<string> {"m", "n", "o"} },
+        { "7", new List<string> {"p", "q", "r", "s"} },
+        { "8", new List<string> {"t", "u", "v"} },
+        { "9", new List<string> {"w", "x", "y", "z"} }
+    };
+
     public IList<string> LetterCombinations(string digits)
     {
-        var lst = new List<string>();
-
-        switch (digits)
-        {
-            case "2": lst = new List<string> {"a", "b", "c"}; break;
-            case "3": lst = new List<string> {"d", "e", "f"}; break;
-            case "4": lst = new List<string> {"g", "h", "i"}; break;
-            case "5": lst = new List<string> {"j", "k", "l"}; break;
-            case "6": lst = new List<string> {"m", "n", "o"}; break;
-            case "7": lst = new List<string> {"p", "q", "r", "s"}; break;
-            case "8": lst = new List<string> {"t", "u", "v"}; break;
-            case "9": lst = new List<string> {"w", "x", "y", "z"}; break;
-        }
-
-        return lst;
+        return GetNumberList(digits);
     }
+
+    private IList<string> GetNumberList(string digit) => 
+        digitToListMap.TryGetValue(digit, out var lst) ? lst : new List<string>();
 }
