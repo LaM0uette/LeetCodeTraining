f3c3eb267f1fe8f86dd3fc51fac1a1846b311791
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-24 14:24:21 +0100

    add first test with crash

--- LeetCodeTrainingTest/Medium/AddTwoNumbersTest.cs
+++ LeetCodeTrainingTest/Medium/AddTwoNumbersTest.cs
@@ -6,6 +6,19 @@ public class AddTwoNumbersTest
 {
     private AddTwoNumbersSolution _solution = new();
 
-    private ListNode GetListNode(ListNode l1, ListNode l2) =>
+    private ListNode GetSolution(ListNode l1, ListNode l2) =>
         _solution.AddTwoNumbers(l1, l2);
+    
+    private static ListNode GetListNode(IEnumerable<int> nums) =>
+        nums.Aggregate((ListNode?)null, (current, num) => new ListNode(num, current));
+    
+    [Fact]
+    public void AddTwoNumbers_00_0()
+    {
+        var l1 = GetListNode(new[] {0});
+        var l2 = GetListNode(new[] {0});
+        
+        var expected = GetListNode(new[] {0});
+        Assert.Equal(expected, GetSolution(l1, l2));
+    }
 }
