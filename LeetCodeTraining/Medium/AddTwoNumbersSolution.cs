3d866ae4cc6da6bbd1058f4678ebbe7a100b808e
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-24 14:13:46 +0100

    add AddTwoNumbersTest

---
+++ LeetCodeTraining/Medium/AddTwoNumbersSolution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodeTraining.Medium;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null!) {
+           this.val = val;
+           this.next = next;
+       }
+}
+
+public class AddTwoNumbersSolution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ LeetCodeTrainingTest/Medium/AddTwoNumbersTest.cs
@@ -0,0 +1,11 @@
+﻿using LeetCodeTraining.Medium;
+
+namespace LeetCodeTrainingTest.Medium;
+
+public class AddTwoNumbersTest
+{
+    private AddTwoNumbersSolution _solution = new();
+
+    private ListNode GetListNode(ListNode l1, ListNode l2) =>
+        _solution.AddTwoNumbers(l1, l2);
+}
