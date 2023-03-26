401473e6638993fb72a0169c822bfaa92b68accc
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-26 13:53:58 +0200

    add LengthOfLongestSubstring_abcabcbb_3 test + ajout de la première potentiel solution

--- LeetCodeTraining/Medium/LongestSubstringWithoutRepeatingCharacters.cs
+++ LeetCodeTraining/Medium/LongestSubstringWithoutRepeatingCharacters.cs
@@ -4,6 +4,23 @@ public class LongestSubstringWithoutRepeatingCharacters
 {
     public int LengthOfLongestSubstring(string s)
     {
-        return 1;
+        var lst = new List<char>();
+        var max = 0;
+        
+        foreach (var sChar in s)
+        {
+            lst.Clear();
+            
+            for (var i = s.IndexOf(sChar); i < s.Length; i++)
+            {
+                if (lst.Any(c => c.Equals(s[i]))) break;
+                lst.Add(s[i]);
+            }
+            
+            var listLength = lst.Count;
+            if (listLength > max) max = listLength;
+        }
+
+        return max;
     }
 }
--- LeetCodeTrainingTest/Medium/LongestSubstringWithoutRepeatingCharactersTest.cs
+++ LeetCodeTrainingTest/Medium/LongestSubstringWithoutRepeatingCharactersTest.cs
@@ -11,5 +11,7 @@ public class LongestSubstringWithoutRepeatingCharactersTest
     private void LengthOfLongestSubstring_bbbbb_1() =>
         Assert.Equal(1, GetLength("bbbbb"));
     
-    
+    [Fact]
+    private void LengthOfLongestSubstring_abcabcbb_3() =>
+        Assert.Equal(3, GetLength("abcabcbb"));
 }
