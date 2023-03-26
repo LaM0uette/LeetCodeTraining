c05fcfda72ac4523399d26a8e822064adb11a361
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-26 14:25:38 +0200

    optimisation des performances en passant par un dico

--- LeetCodeTraining/Medium/LongestSubstringWithoutRepeatingCharacters.cs
+++ LeetCodeTraining/Medium/LongestSubstringWithoutRepeatingCharacters.cs
@@ -1,41 +1,29 @@
-﻿using System.Text.RegularExpressions;
-
-namespace LeetCodeTraining.Medium;
+﻿namespace LeetCodeTraining.Medium;
 
 public class LongestSubstringWithoutRepeatingCharacters
 {
     public int LengthOfLongestSubstring(string s)
     {
         if (!CheckConstraints(s)) return 0;
-        
-        var lst = new List<char>();
-        var max = 0;
-        
-        foreach (var sChar in s)
-        {
-            lst.Clear();
-            
-            for (var i = s.IndexOf(sChar); i < s.Length; i++)
-            {
-                if (lst.Any(c => c.Equals(s[i]))) break;
-                lst.Add(s[i]);
-            }
-            
-            var listLength = lst.Count;
-            if (listLength > max) max = listLength;
-        }
 
+        var max = 0;
+        var left = 0;
+        var right = 0;
+        var charIndices = new Dictionary<char, int>();
+
+        while (right < s.Length)
+        {
+            var currentChar = s[right];
+
+            if (charIndices.ContainsKey(currentChar) && charIndices[currentChar] >= left)
+                left = charIndices[currentChar] + 1;
+
+            charIndices[currentChar] = right;
+            max = Math.Max(max, right - left + 1);
+            right++;
+        }
         return max;
     }
-    
-    static bool CheckConstraints(string s)
-    {
-        // Vérifiez que la longueur de la chaîne est comprise entre 0 et 5 * 10^4
-        if (s.Length < 0 || s.Length > 5 * Math.Pow(10, 4)) 
-            return false;
 
-        // Vérifiez que la chaîne contient uniquement des lettres anglaises, des chiffres, des symboles et des espaces
-        var validCharacters = new Regex(@"^[a-zA-Z0-9\s!@#$%^&*()_+{}|;':"",./<>?`~[\]\\-]*$");
-        return validCharacters.IsMatch(s);
-    }
+    private static bool CheckConstraints(string s) => !(s.Length > 5 * Math.Pow(10, 4));
 }
