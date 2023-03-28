45c267b8f8bfea16f00261d4b5755c4ea9ece708
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-28 14:59:09 +0200

    paased all tests

--- LeetCodeTraining/Medium/StringToInteger.cs
+++ LeetCodeTraining/Medium/StringToInteger.cs
@@ -17,7 +17,26 @@ public class StringToInteger
     public int MyAtoi(string s)
     {
         if (s.Equals("")) return 0;
+        
+        int i = 0, sign = 1, lenght = s.Length;
+        long result = 0;
 
-        throw new NotImplementedException();
+        while (i < lenght && s[i].Equals(' ')) i++;
+
+        if (s[i].Equals('-') || s[i].Equals('+'))
+            sign = s[i++].Equals('-') ? -1 : 1;
+
+        while (i < lenght && char.IsDigit(s[i]))
+        {
+            result = result * 10 + (s[i++] - '0');
+            
+            switch (result * sign)
+            {
+                case > int.MaxValue: return int.MaxValue;
+                case < int.MinValue: return int.MinValue;
+            }
+        }
+
+        return (int)(result * sign);
     }
 }
