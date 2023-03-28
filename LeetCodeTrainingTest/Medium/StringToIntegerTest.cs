085fc7a0005e2cdf074c13d73022ab1227262a2d
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-28 09:35:55 +0200

    add all test

--- LeetCodeTrainingTest/Medium/StringToIntegerTest.cs
+++ LeetCodeTrainingTest/Medium/StringToIntegerTest.cs
@@ -20,4 +20,55 @@ public class StringToIntegerTest
         Assert.Equal(-42, GetAtoi(" -42"));
         Assert.Equal(+42, GetAtoi(" +42"));
     }
+    
+    [Fact]
+    private void MyAtoi_SpaceArround101_101()
+    {
+        Assert.Equal(101, GetAtoi(" 101 "));
+        Assert.Equal(-101, GetAtoi(" -101 "));
+        Assert.Equal(+101, GetAtoi(" +101 "));
+    }
+    
+    [Fact]
+    private void MyAtoi_OnlyChar_0()
+    {
+        Assert.Equal(0, GetAtoi("wordsonly"));
+        Assert.Equal(0, GetAtoi(" wordsonly"));
+        Assert.Equal(0, GetAtoi(" wordsonly "));
+    }
+    
+    [Fact]
+    private void MyAtoi_CharInt_Int()
+    {
+        Assert.Equal(4193, GetAtoi("4193 with words"));
+        Assert.Equal(0, GetAtoi("words and 987"));
+        Assert.Equal(-42, GetAtoi(" -42abc "));
+    }
+    
+    [Fact]
+    private void MyAtoi_DoubleSigns1_1()
+    {
+        Assert.Equal(0, GetAtoi("-+1"));
+        Assert.Equal(0, GetAtoi("+-1"));
+        Assert.Equal(0, GetAtoi("++1"));
+        Assert.Equal(0, GetAtoi("--1"));
+        Assert.Equal(0, GetAtoi(" -+1"));
+        Assert.Equal(0, GetAtoi(" +-1"));
+        Assert.Equal(0, GetAtoi(" ++1"));
+        Assert.Equal(0, GetAtoi(" --1"));
+    }
+    
+    [Fact]
+    private void MyAtoi_LimitInt32_0()
+    {
+        Assert.Equal(2147483647, GetAtoi("2147483648"));
+        Assert.Equal(-2147483648, GetAtoi("-2147483649"));
+    }
+    
+    [Fact]
+    private void MyAtoi_LimitInt32_Int()
+    {
+        Assert.Equal(2147483647, GetAtoi("2147483647"));
+        Assert.Equal(-2147483648, GetAtoi("-2147483648"));
+    }
 }
