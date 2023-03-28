0ab0ad4f42b65b74c5c428064479393edc83b444
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-28 09:18:59 +0200

    add first tests with regex solution

---
+++ LeetCodeTraining/Medium/StringToInteger.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace LeetCodeTraining.Medium;
+
+public class StringToInteger
+{
+    public int MyAtoi(string s) {
+        
+        var m = Regex.Match(s.TrimStart(), @"^([-+]?\d+)");
+        
+        if (!m.Success) return 0;
+        if (int.TryParse(m.Groups[0].Value, out var result)) return result;
+        
+        return m.Groups[0].Value.StartsWith("-") ? int.MinValue : int.MaxValue;
+    }
+}
---
+++ LeetCodeTrainingTest/Medium/StringToIntegerTest.cs
@@ -0,0 +1,23 @@
+﻿using LeetCodeTraining.Medium;
+
+namespace LeetCodeTrainingTest.Medium;
+
+public class StringToIntegerTest
+{
+    private StringToInteger _stringToInteger = new();
+    private int GetAtoi(string s) => _stringToInteger.MyAtoi(s);
+    
+    [Fact]
+    private void MyAtoi_WithEmptyString_ReturnsZero()
+    {
+        Assert.Equal(0, GetAtoi(""));
+    }
+    
+    [Fact]
+    private void MyAtoi_Space42_42()
+    {
+        Assert.Equal(42, GetAtoi(" 42"));
+        Assert.Equal(-42, GetAtoi(" -42"));
+        Assert.Equal(+42, GetAtoi(" +42"));
+    }
+}
