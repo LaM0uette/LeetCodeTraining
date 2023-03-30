7172776502d3f2b2f9c70b6baa90c29ca92915e2
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-03-30 08:55:59 +0200

    all tests passed

--- LeetCodeTraining/Easy/FizzBuzzSolution.cs
+++ LeetCodeTraining/Easy/FizzBuzzSolution.cs
@@ -4,6 +4,33 @@ public class FizzBuzzSolution
 {
     public IList<string> FizzBuzz(int n)
     {
-        throw new NotImplementedException();
+        var result = new List<string>();
+        
+        if (n is <= 0 or > 10000) return result;
+        
+        for (var i = 1; i <= n; i++)
+        {
+            if ((i % 3).Equals(0) && (i % 5).Equals(0))
+            {
+                result.Add("FizzBuzz"); 
+                continue;
+            }
+            
+            if ((i % 3).Equals(0))
+            {
+                result.Add("Fizz"); 
+                continue;
+            }
+            
+            if ((i % 5).Equals(0))
+            {
+                result.Add("Buzz"); 
+                continue;
+            }
+
+            result.Add(i.ToString());
+        }
+
+        return result;
     }
 }
--- LeetCodeTrainingTest/Easy/FizzBuzzTest.cs
+++ LeetCodeTrainingTest/Easy/FizzBuzzTest.cs
@@ -13,4 +13,67 @@ public class FizzBuzzTest
         var result = GetFizz(0);
         Assert.Empty(result);
     }
+    
+    [Fact]
+    private void FizzBuzz_10001_ReturnsEmpty()
+    {
+        var result = GetFizz(10001);
+        Assert.Empty(result);
+    }
+    
+    [Fact]
+    private void FizzBuzz_1_1()
+    {
+        var expected = new List<string> {"1"};
+        var result = GetFizz(1);
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    private void FizzBuzz_2_12()
+    {
+        var expected = new List<string> {"1", "2"};
+        var result = GetFizz(2);
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    private void FizzBuzz_3_12Fizz()
+    {
+        var expected = new List<string> {"1","2","Fizz"};
+        var result = GetFizz(3);
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    private void FizzBuzz_4_12Fizz4()
+    {
+        var expected = new List<string> {"1","2","Fizz", "4"};
+        var result = GetFizz(4);
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    private void FizzBuzz_5_12Fizz4Buzz()
+    {
+        var expected = new List<string> {"1","2","Fizz","4","Buzz"};
+        var result = GetFizz(5);
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    private void FizzBuzz_6_12Fizz4BuzzFizz()
+    {
+        var expected = new List<string> {"1","2","Fizz","4","Buzz", "Fizz"};
+        var result = GetFizz(6);
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    private void FizzBuzz_15_12Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz()
+    {
+        var expected = new List<string> {"1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"};
+        var result = GetFizz(15);
+        Assert.Equal(expected, result);
+    }
 }
