44e5dcb24e2f5978a8679a2fc2bd512064e96d53
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-06-16 19:41:35 +0200

    add LongestCommonPrefix test

---
+++ LeetCodeTraining/Easy/LongestCommonPrefix.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeTraining.Easy;
+
+public class Solution
+{
+    public string LongestCommonPrefix(string[] lst)
+    {
+        if (lst.Length <= 0) return "";
+        
+        var iLenght = lst[0].Length;
+        var prefix = "";
+
+        for (var i = 0; i < iLenght;i++)
+        {
+            foreach (var str in lst)
+            {
+                var currentPrefix = str[..i];
+
+                if (!currentPrefix.Equals(prefix) && !prefix.Equals("")) return "";
+            
+                prefix = currentPrefix;
+            
+                i++;
+            }
+        }
+
+        return prefix;
+    }
+}
---
+++ LeetCodeTrainingTest/Easy/LongestCommonPrefixTest.cs
@@ -0,0 +1,36 @@
+﻿using LeetCodeTraining.Easy;
+
+namespace LeetCodeTrainingTest.Easy;
+
+public class LongestCommonPrefixTest
+{
+    [Fact]
+    public void LongestCommonPrefix_Empty_EmptyString()
+    {
+        var lst = Array.Empty<string>();
+        
+        var result = new Solution().LongestCommonPrefix(lst);
+        
+        Assert.Equal("", result);
+    }
+    
+    [Fact]
+    public void LongestCommonPrefix_FlowerFlowFlight_Fl()
+    {
+        var lst = new[] { "flower","flow","flight" };
+        
+        var result = new Solution().LongestCommonPrefix(lst);
+        
+        Assert.Equal("fl", result);
+    }
+    
+    [Fact]
+    public void LongestCommonPrefix_DogRacecarCar_EmptyString()
+    {
+        var lst = new[] { "dog","racecar","car" };
+        
+        var result = new Solution().LongestCommonPrefix(lst);
+        
+        Assert.Equal("", result);
+    }
+}
