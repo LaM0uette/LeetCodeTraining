bdf0ea9d270b3f5fda62e4b840b57d91e3261b95
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-06-18 21:21:58 +0200

    add ValidParenthesesTest

---
+++ LeetCodeTraining/Easy/ValidParentheses.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCodeTraining.Easy;
+
+public class ValidParentheses
+{
+    public bool IsValid(string s)
+    {
+        return s.Length <= 0 ? false : true;
+    }
+}
---
+++ LeetCodeTrainingTest/Easy/ValidParenthesesTest.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeTraining.Easy;
+
+namespace LeetCodeTrainingTest.Easy;
+
+public class ValidParenthesesTest
+{
+    [Fact]
+    public void IsValid_Emptysting_False()
+    {
+        const string s = "";
+        var validParentheses = new ValidParentheses();
+        
+        var isValid = validParentheses.IsValid(s);
+        
+        Assert.False(isValid);
+    }
+    
+    [Fact]
+    public void IsValid_PoPf_True()
+    {
+        const string s = "()";
+        var validParentheses = new ValidParentheses();
+        
+        var isValid = validParentheses.IsValid(s);
+        
+        Assert.True(isValid);
+    }
+}
