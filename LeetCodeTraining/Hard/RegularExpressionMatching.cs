5b564658d697982b32295ed697e56f7347c0e93a
LaM0uette <LaM0uette.dev@gmail.com>
2023-06-19 13:25:54 +0200

    add RegularExpressionMatching test

---
+++ LeetCodeTraining/Hard/RegularExpressionMatching.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCodeTraining.Hard;
+
+public class RegularExpressionMatching
+{
+    public bool IsMatch(string s, string p)
+    {
+        return true;
+    }
+}
---
+++ LeetCodeTrainingTest/Hard/RegularExpressionMatchingTest.cs
@@ -0,0 +1,17 @@
+﻿using LeetCodeTraining.Hard;
+
+namespace LeetCodeTrainingTest.Hard;
+
+public class RegularExpressionMatchingTest
+{
+    [Fact]
+    public void IsMatch_EmptyEmpty_True()
+    {
+        const string s = "";
+        const string p = "";
+        
+        var isMatch = new RegularExpressionMatching();
+        
+        Assert.True(isMatch.IsMatch(s, p));
+    }
+}
