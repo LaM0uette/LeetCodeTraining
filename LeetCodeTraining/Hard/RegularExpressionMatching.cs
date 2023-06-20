2dc765924d8af0deaa29c7f8799b40a9f248344b
LaM0uette <LaM0uette.dev@gmail.com>
2023-06-20 11:46:54 +0200

    add all tests

--- LeetCodeTraining/Hard/RegularExpressionMatching.cs
+++ LeetCodeTraining/Hard/RegularExpressionMatching.cs
@@ -2,8 +2,23 @@
 
 public class RegularExpressionMatching
 {
-    public bool IsMatch(string s, string p)
-    {
-        return s.Equals(p);
+    public bool IsMatch(string s, string p) {
+        var dp = new bool[s.Length+1, p.Length+1];
+        dp[s.Length, p.Length] = true;
+
+        for (var i = s.Length; i >= 0; i--)
+        {
+            for (var j = p.Length - 1; j >= 0; j--)
+            {
+                var firstMatch = i < s.Length && (p[j].Equals(s[i]) || p[j].Equals('.'));
+
+                if (j+1 < p.Length && p[j+1].Equals('*')){
+                    dp[i,j] = dp[i,j+2] || firstMatch && dp[i+1,j];
+                } else {
+                    dp[i,j] = firstMatch && dp[i+1,j+1];
+                }
+            }
+        }
+        return dp[0,0];
     }
 }
--- LeetCodeTrainingTest/Hard/RegularExpressionMatchingTest.cs
+++ LeetCodeTrainingTest/Hard/RegularExpressionMatchingTest.cs
@@ -48,5 +48,58 @@ public class RegularExpressionMatchingTest
         Assert.False(isMatch.IsMatch(s, p));
     }
     
+    [Fact]
+    public void IsMatch_aDot_True()
+    {
+        const string s = "a";
+        const string p = ".";
+        
+        var isMatch = new RegularExpressionMatching();
+        
+        Assert.True(isMatch.IsMatch(s, p));
+    }
     
+    [Fact]
+    public void IsMatch_aaaaaa_True()
+    {
+        const string s = "aaa";
+        const string p = "aaa";
+        
+        var isMatch = new RegularExpressionMatching();
+        
+        Assert.True(isMatch.IsMatch(s, p));
+    }
+    
+    [Fact]
+    public void IsMatch_aaaaEE_True()
+    {
+        const string s = "aaa";
+        const string p = "a*";
+        
+        var isMatch = new RegularExpressionMatching();
+        
+        Assert.True(isMatch.IsMatch(s, p));
+    }
+    
+    [Fact]
+    public void IsMatch_aaabEE_False()
+    {
+        const string s = "aaa";
+        const string p = "b*";
+        
+        var isMatch = new RegularExpressionMatching();
+        
+        Assert.False(isMatch.IsMatch(s, p));
+    }
+    
+    [Fact]
+    public void IsMatch_abab_True()
+    {
+        const string s = "ab";
+        const string p = "ab";
+        
+        var isMatch = new RegularExpressionMatching();
+        
+        Assert.True(isMatch.IsMatch(s, p));
+    }
 }
